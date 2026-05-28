29415c45cbcb526af1d96c705c9e3e049ad17e69
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-05-28 15:47:00 +0700

    iterasi dari posisi FRONT hingga akhir array

--- queue.cpp
+++ queue.cpp
@@ -90,5 +90,16 @@ public:
             }
             cout << endl;
         }
+
+        else
+        {
+            while (FRONT_Position <= max - 1)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+
+
+        }
     }
 };
