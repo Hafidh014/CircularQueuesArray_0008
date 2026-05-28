c0723075ad5981d5822a97277890f7b93f4a3a15
muhammad.hafidh2006@gmail.com <muhammad.hafidh2006@gmail.com>
2026-05-28 15:48:01 +0700

    iterasi dari awal array hingga REAR

--- queue.cpp
+++ queue.cpp
@@ -99,7 +99,14 @@ public:
                 FRONT_Position++;
             }
 
-
+            FRONT_Position = 0;
+            
+            while (FRONT_Position <= REAR_Position)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+            cout << endl;
         }
     }
 };
